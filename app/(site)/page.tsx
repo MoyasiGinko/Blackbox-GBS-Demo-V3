9d5ee709b30a9f9da45964a453897006d31435a5
moyasiginko <mrsiyam61@gmail.com>
2025-05-28 07:30:21 +0600

    restructure themes

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect } from "react";
-import Navbar from "../components/Navbar";
+import Navbar from "@/components/Navbar";
 
 export default function HomePage() {
   return (
